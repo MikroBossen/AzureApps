5405c52f307a96e2182531cb01ba880bb497330a
Admin <Admin>
2016-09-16 15:23:20 +0200

    Initial commit, whee!

---
+++ bossapp/bossapp_Xamarin_Forms/bossapp/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace bossapp
+{
+	public static class Constants
+	{
+		// Replace strings with your mobile services and gateway URLs.
+		public static string ApplicationURL = @"https://bossapp.azurewebsites.net";
+	}
+}
+
---
+++ bossapp/bossapp_Xamarin_Forms/bossapp/TodoItem.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.WindowsAzure.MobileServices;
+using Newtonsoft.Json;
+
+namespace bossapp
+{
+	public class TodoItem
+	{
+		string id;
+		string name;
+		bool done;
+        string category;
+
+		[JsonProperty(PropertyName = "id")]
+		public string Id
+		{
+			get { return id; }
+			set { id = value;}
+		}
+
+		[JsonProperty(PropertyName = "text")]
+		public string Name
+		{
+			get { return name; }
+			set { name = value;}
+		}
+
+		[JsonProperty(PropertyName = "complete")]
+		public bool Done
+		{
+			get { return done; }
+			set { done = value;}
+		}
+
+        [JsonProperty(PropertyName = "category")]
+        public string Category
+        {
+            get { return this.category; }
+            set { this.category = value; }
+        }
+
+        [Version]
+        public string Version { get; set; }
+	}
+}
+
